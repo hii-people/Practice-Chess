ebaf0241b59c7b2d8c9ad4d8840cc4255e5552b3
hii-people <116594295+hii-people@users.noreply.github.com>
2023-05-07 21:46:33 +0100

    Update BoardManagerScript.cs

--- 3D-Practice-Chess/Assets/Scripts/BoardManagerScript.cs
+++ 3D-Practice-Chess/Assets/Scripts/BoardManagerScript.cs
@@ -18,7 +18,7 @@ public class BoardManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChessPiecePosition = new ChessPieces[8, 8];
     }
 
     // Update is called once per frame
